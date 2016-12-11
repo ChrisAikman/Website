2ecaff1a43445690eee3d1078a09c7e9d16e199d
Chris Aikman <cdaikman@gmail.com>
2016-12-10 23:00:27 -0700

    Initial setup.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Website
 Repository for my website.
+
+##Tools
+Tools used are Middleman, HAML, SASS, and JavaScript.
---
+++ config.rb
@@ -0,0 +1,28 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+
+# General configuration
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+end
